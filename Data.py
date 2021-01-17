701c0259f9b37ef5710e5a7afc7ca4b000409a0c
Rohit <76251106+parallel-ai@users.noreply.github.com>
2021-01-17 14:41:16 +0530

    Delete Data.py

--- Data.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Data(object):
-    def __init__(self):
-        self.resp = ''
-    @staticmethod
-    def data(filePath):
-        text = textract.process(filePath)
-        return text
